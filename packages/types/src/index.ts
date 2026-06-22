ce12596807810b5f35048603ee0a2b650f92d350
raj248 <shashankraj007281@gmail.com>
2026-06-22 12:33:03 +0530

    INIT

---
+++ packages/types/src/index.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: Date;
+}
+
+export interface ApiResponse<T> {
+  success: boolean;
+  data: T;
+  error?: string;
+}
